ada6b21d0ed457dd9d8e47c4ab8895b4023f280f
Martin Kapp <martin@coderats.net>
2017-04-23 00:57:19 +0300

    Setup fix

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 BASEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-echo "$BASEDIR"
+PLATFORM=$(uname)
 
 # Prep folders
 mkdir -p $BASEDIR/local
@@ -12,12 +12,12 @@ cd $BASEDIR
 sudo -v
 
 # If the platform is Linux, try an apt-get to install zsh and then recurse
-if [[ $platform == 'Linux' ]]; then
+if [[ $PLATFORM == 'Linux' ]]; then
   if [ -f /etc/debian_version ]; then
     ./linux/setup.sh
   fi
 
 # If the platform is OS X, tell the user to install zsh :)
-elif [[ $platform == 'Darwin' ]]; then
+elif [[ $PLATFORM == 'Darwin' ]]; then
   ./osx/setup.sh
 fi
