27461b561ecd6985acd7e533de7d559d69e1d64b
Martin Kapp <martin@coderats.net>
2017-04-22 23:01:13 +0300

    Add scipt hash

--- linux/code.sh
+++ linux/code.sh
@@ -1,4 +1,6 @@
-if [ ! -f /etc/apt/sources.list.d/vscode.list ]; then
+#!/usr/bin/bash
+
+if [[ ! -f /etc/apt/sources.list.d/vscode.list ]]; then
   curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
   sudo mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
   sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
