73579b8426debe8a0ebd172632476d6d3c88d04b
Martin Kapp <martin@coderats.net>
2017-04-22 23:10:05 +0300

    Fix if file exists syntax

--- linux/code.sh
+++ linux/code.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 
-if [[ ! -f /etc/apt/sources.list.d/vscode.list ]]; then
+if [ ! -f /etc/apt/sources.list.d/vscode.list ]; then
   curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
   sudo mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
   sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
