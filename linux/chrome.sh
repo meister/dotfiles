90a8729d583274c0330d0299e257def9721bd65f
Martin Kapp <martin@coderats.net>
2017-04-22 22:57:23 +0300

    Update VS Code and Chrome install scripts for linux

--- linux/chrome.sh
+++ linux/chrome.sh
@@ -1,5 +1,9 @@
-wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
-sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
+if [ ! -f /etc/apt/sources.list.d/google.list ]; then
+  wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+  sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
+fi
 
 sudo apt-get update
 sudo apt-get install -y google-chrome-stable
+
+echo "[PlankDockItemPreferences]\nLauncher=file:///usr/share/applications/google-chrome.desktop" > ~/.plank/dock1/launchers/google-chrome.dockitem
---
+++ linux/code.sh
@@ -0,0 +1,10 @@
+if [ ! -f /etc/apt/sources.list.d/vscode.list ]; then
+  curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
+  sudo mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
+  sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+fi
+
+sudo apt-get update
+sudo apt-get install -y code # or code-insiders
+
+echo "[PlankDockItemPreferences]\nLauncher=file:///usr/share/applications/code.desktop" >  ~/.plank/dock1/launchers/code.dockitem
