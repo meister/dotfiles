3136949343fe93becb017d666682e0268f6d9b67
Martin Kapp <martin@coderats.net>
2017-07-06 01:13:18 +0300

    Fix zsh configure path

--- osx/setup.sh
+++ osx/setup.sh
@@ -21,4 +21,4 @@ curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.1/install.sh | b
 brew install zsh zsh-completions
 
 # Configure Zsh
-source $BASEDIR/configure-zsh.sh
+source $BASEDIR/zsh/configure.sh
