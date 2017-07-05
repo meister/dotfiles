75626bee04aa4f90d1940fd82e2efea698d84b50
Martin Kapp <martin@coderats.net>
2017-07-06 00:07:09 +0300

    Install script fixes

--- install.sh
+++ install.sh
@@ -1,19 +1,21 @@
 #!/usr/bin/env bash
 
+set -e
+
 [[ -x `command -v wget` ]] && CMD="wget --no-check-certificate -O -"
 [[ -x `command -v curl` ]] >/dev/null 2>&1 && CMD="curl -#L"
 
 function has_git() {
   if [[ -x `command -v git` ]]; then
-    if `git --version`; then
-      exit 0
+    if `git --version >/dev/null 2>&1 `; then
+      return 0
     fi
   fi
 
-  exit 1
+  return 1
 }
 
-if [ -z "$CMD" ]; then
+if [ -z "${CMD}" ]; then
   echo "No curl or wget available. Aborting."
 else
   echo "Installing dotfiles"
@@ -23,10 +25,12 @@ else
   fi
 
   if has_git; then
+    echo "Using Git to download scripts"
     eval "git clone --recursive https://github.com/meister/dotfiles ~/.dotfiles"
   else
+    echo "Using ${CMD} to download scripts"
     mkdir -p ~/.dotfiles
-    eval "$CMD https://github.com/meister/dotfiles/tarball/master | tar -xzv -C ~/.dotfiles --strip-components=1 --exclude='{.gitignore}'"
+    eval "${CMD} https://github.com/meister/dotfiles/tarball/master | tar -xzv -C ~/.dotfiles --strip-components=1 --exclude='{.gitignore}'"
   fi
 
   source ~/.dotfiles/setup.sh
