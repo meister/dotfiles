c1ff118dc5cb1e9ef1121f2b663d7ccf150ca8a9
Martin Kapp <martin@coderats.net>
2021-02-02 23:34:21 +0200

    Disable VIM config for now

--- editors/vim/configure.sh
+++ editors/vim/configure.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+# @TODO Fix VIM Config
+return
+
 # Update Vundle
 if [ ! -d ~/.vim/bundle/Vundle.vim ]; then
   git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
